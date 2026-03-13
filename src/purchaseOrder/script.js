4208bb7d991bbb10f8e7cef0ccd09f8cdea32901
DJE-crypto <ghiles.djennane@se.univ-bejaia.dz>
2026-03-13 12:44:59 +0100

    Delete src directory

--- src/purchaseOrder/script.js
+++
@@ -1,5 +0,0 @@
-const orderDateEl = document.getElementById("orderDate");
-const deliveryDateEl = document.getElementById("deliveryDate");
-const totalPriceEl = document.getElementById("totalPrice");
-const paymentMethodEl = document.getElementById("paymentMethod");
-const paymentStatusEl = document.getElementById("paymentStatus");
