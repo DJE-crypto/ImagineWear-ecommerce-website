a372ca7411856bd146191b091adc219814763973
DJE-crypto <ghiles.djennane@se.univ-bejaia.dz>
2026-03-13 12:29:39 +0100

    Add files via upload

---
+++ src/purchaseOrder/script.js
@@ -0,0 +1,5 @@
+const orderDateEl = document.getElementById("orderDate");
+const deliveryDateEl = document.getElementById("deliveryDate");
+const totalPriceEl = document.getElementById("totalPrice");
+const paymentMethodEl = document.getElementById("paymentMethod");
+const paymentStatusEl = document.getElementById("paymentStatus");
